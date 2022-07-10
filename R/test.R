23f8180db4d19a9e960bb191af713fb98e686626
Stefan <stefan@alphaorange.de>
2022-07-10 19:53:04 +0200

    reduced clusters

--- R/test.R
+++ R/test.R
@@ -2,10 +2,15 @@
 
 source("R/vibratr.R")
 
-path <- "test1.jpg"
+path <- "test4.jpg"
 
-pal <- .vibrance(path, bw = TRUE) %>%
-  mutate(bubble = 28 + Size / max(Size) * 100)
+# Simple 3-color palette
+cols <- generate_palette(path)
+barplot(rep(1, 3), col = cols)
+
+# Color decoding
+pal <- .vibrance(path, bw = FALSE) %>%
+  mutate(bubble = sqrt(Size / max(Size)) * 128)
 plot_ly(pal, x = ~Vibrance, y = ~L, text = ~RGB,
         type = 'scatter', mode = 'markers',
         marker = list(size = ~bubble, opacity = 1, color = ~RGB,
@@ -16,3 +21,27 @@ plot_ly(pal, x = ~Vibrance, y = ~L, text = ~RGB,
 
 # R/G/B pure score too dark
 # cluster centers are not pure enough
+
+
+
+library(cluster)
+
+results <- cbind(Cluster = 1:nrow(results), results)
+resRGB <- results %>% select(R, G, B)
+dist_mat <- dist(resRGB)
+hclust_avg <- hclust(dist_mat, method = 'complete')
+plot(hclust_avg)
+cutno <- sum(hclust_avg$height > 0.25) + 1
+cuts <- cutree(hclust_avg, k = cutno)
+cbind(results, FinalCluster = cuts)
+
+
+
+clusters <- agnes(resRGB)
+plot(clusters)
+
+#calculate gap statistic for each number of clusters (up to 10 clusters)
+gap_stat <- clusGap(resRGB, FUN = hcut, nstart = 25, K.max = 20, B = 50)
+
+#produce plot of clusters vs. gap statistic
+fviz_gap_stat(gap_stat)
--- R/vibratr.R
+++ R/vibratr.R
@@ -3,6 +3,19 @@ library(dplyr)
 library(tidyr)
 library(plotly)
 
+# Things TODO:
+# Perceived Luminance (blue should no longer rank higher as green)
+#   https://stackoverflow.com/questions/596216/formula-to-determine-perceived-brightness-of-rgb-color
+# Free clustering (no more ten clusters of black)
+# Palette generation
+# Better definition of vibrance
+# problem in clustering: different hues of near-black are far distant to each other
+#   => no clean black or white clusters
+# from each cluster take 90%-quantile most vibrant as representative
+# generate_palette should return harmonic set of differents hues
+#   or find corresponding color in actual image to avoid smearing
+
+
 
 # Calculate vibrance from HSL
 vibrance_HSL <- function(H, S, L, bw = FALSE) {
@@ -60,7 +73,7 @@ depolar <- function(H1, H2) {
 
 
 # Generate palette from image
-.vibrance <- function(path, rescale = 100000, bw = FALSE) {
+.vibrance <- function(path, rescale = 50000, bw = FALSE) {
 
   # load image and rescale
   img_rgb <- load.image(path)
@@ -92,7 +105,7 @@ depolar <- function(H1, H2) {
   df_cluster <- df_total %>% select(H1, H2, S, L)
   km <- kmeans(df_cluster, centers = 20)
 
-  # Threshold for tiny clusters
+  # Remove tiny clusters
   min_size <- nrow(df_total) / 1000
 
   # Bind RGB and Vibrance to centers
@@ -109,13 +122,34 @@ depolar <- function(H1, H2) {
     rgb(row[5], row[6], row[7])
   })
 
+  # Reduce number of clusters
+  results_RGB <- results %>% select(R, G, B)
+  dist_mat <- dist(results_RGB)
+  clusters <- hclust(dist_mat, method = 'complete')
+  cut <- max(sum(clusters$height > 0.25) + 1, 6) # min clusters: 6
+  clusters <- cutree(clusters, k = cut)
+  results <- cbind(results, Cluster = clusters) %>%
+    group_by(Cluster) %>%
+    arrange(desc(Size)) %>%
+    mutate(Size = sum(Size)) %>%
+    filter(row_number() == 1) %>%
+    ungroup
+
   # Classify for luminance and vibrance
   results <- results %>%
-    mutate(Vib_class = ifelse(Vibrance > 1/3, "vibrant",
-                       ifelse(Vibrance > 1/6, "muted",
+    mutate(Vib_class = ifelse(Vibrance > 0.6, "vibrant",
+                       ifelse(Vibrance > 0.4, "muted",
                                               "grey"))) %>%
-    mutate(Lum_class = ifelse(L > 1/2, "light", "dark"))
+    mutate(Lum_class = ifelse(L > 0.5, "light", "dark"))
 
   results
 
+}
+
+# TODO deconstruct def
+generate_palette <- function(path, n = 3) {
+  .vibrance(path) %>%
+    arrange(desc(Vibrance)) %>%
+    pull(RGB) %>%
+    `[`(1:n)
 }
